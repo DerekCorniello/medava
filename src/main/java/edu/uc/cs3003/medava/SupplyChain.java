ae795d9632f012679e40e372a19ece25ea590134
XDCoolMan <dcorniello.business@gmail.com>
2023-11-12 17:13:25 -0500

    Implemented supply chain superclass

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -1,6 +1,20 @@
 package edu.uc.cs3003.medava;
 
-public class SupplyChain {
-    public static void main(String args[]) {
+public class SupplyChain 
+{
+    public static void main(String args[])
+    {
+        //System.out.println("Hello, Derek! This is the WOPR speaking.");
+
+        class Medicine
+        {
+            private String mMedicineName;
+
+            public Medicine(String medicineName) 
+            {
+                mMedicineName = medicineName;
+            }
+        }
+        
     }
 }
