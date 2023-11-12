4cafe55b54e0041ca64647467b6dfd3af66d4029
XDCoolMan <dcorniello.business@gmail.com>
2023-11-12 17:28:15 -0500

    Began implementation of transporter

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,23 @@
 package edu.uc.cs3003.medava;
-public class Transporter {
+import java.util.List;
+import java.util.ArrayList;
+
+public class Transporter 
+{
+    private String mTransporterName;
+    public List<Medicine> goods;
+
+    {
+        goods = new ArrayList<Medicine>();   
+    }
+
+    public String getTransporterName() 
+    {
+        return mTransporterName;
+    }
+
+    public void ship() 
+    {
+        // Do some shipping!
+    }
 }
