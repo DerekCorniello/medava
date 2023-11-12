b38bfbb718ac093504d47fb9894eff54c282b935
XDCoolMan <dcorniello.business@gmail.com>
2023-11-12 17:34:54 -0500

    Implemented Hospital Class

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,1 +1,31 @@
 package edu.uc.cs3003.medava;
+
+public class Hospital 
+{
+
+    private String name;
+
+    // Hospital Constructor
+    public Hospital(String hospitalName) 
+    {
+        name = hospitalName;
+    }
+
+    // Member Function: receive
+    // 'unloads' a transporter full with medicine
+    // Inputs: a transporter
+    // No output
+    void receive(Transporter t) 
+    {
+        while (!t.goods.isEmpty()) {
+            Medicine unloaded = t.goods.remove(0);
+            System.out.println(String.format("Receiving an %s.", unloaded.getMedicineName()));
+        }
+    }
+
+    // Getter function for name
+    public String name() 
+    {
+        return name;
+    }
+}
