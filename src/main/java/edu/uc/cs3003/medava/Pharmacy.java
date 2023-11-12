ee773b04d3fde50479c04c89116a73e1dfe28208
XDCoolMan <dcorniello.business@gmail.com>
2023-11-12 17:35:06 -0500

    Implemented Pharmacy Class

--- src/main/java/edu/uc/cs3003/medava/Pharmacy.java
+++ src/main/java/edu/uc/cs3003/medava/Pharmacy.java
@@ -1,1 +1,30 @@
 package edu.uc.cs3003.medava;
+
+public class Pharmacy 
+{
+
+    // Pharmacy constructor
+    public Pharmacy(String pharmacyName)
+    {
+        mPharmacyName = pharmacyName;
+    }
+
+    // Member Function: send
+    // 'sends' the transporter to the hospital
+    // Inputs: string of medicine name
+    // Outputs: boolean of if successful or not
+    public boolean send(Transporter t) 
+    {
+        Medicine advil = new Medicine("Advil");
+        System.out.println(String.format("Sending an %s.", advil.getMedicineName()));
+        return t.goods.add(advil);
+    }
+
+    // Getter function for name of pharmacy
+    public String pharmacyName() 
+    {
+        return mPharmacyName;
+    }
+
+    private String mPharmacyName;
+}
