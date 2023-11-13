01aacec188afcb5524115f7d53fef4e43b293db6
XDCoolMan <dcorniello.business@gmail.com>
2023-11-12 20:03:09 -0500

    Checkpoint 1

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -5,5 +5,6 @@ public class SupplyChain
     public static void main(String args[])
     {
         //System.out.println("Hello, Derek! This is the WOPR speaking.");
+        HospitalRunner.run();
     }
 }
